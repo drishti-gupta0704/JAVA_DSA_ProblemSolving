7fbe47aa68e5d331706d9f4bb36cb148447864dc
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-04-03 22:31:28 +0530

    solved a question using greedy approach

---
+++ src/greedy/LemonadeChange.java
@@ -0,0 +1,41 @@
+package greedy;
+
+public class LemonadeChange {
+    public static boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            }
+            else if (bill == 10) {
+                if (five == 0) {
+                    return false;
+                }
+                five--;
+                ten++;
+            }
+            else {
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] bills = {5, 5, 5, 10, 20};
+
+        boolean result = lemonadeChange(bills);
+        System.out.println("Can give change: " + result);
+    }
+}
