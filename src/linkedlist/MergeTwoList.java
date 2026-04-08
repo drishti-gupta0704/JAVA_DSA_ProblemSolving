4376e7d926289ff2c83ba9027aad94f09bff1c00
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-04-08 21:11:37 +0530

    solved merge LL question

---
+++ src/linkedlist/MergeTwoList.java
@@ -0,0 +1,55 @@
+package linkedlist;
+
+public class MergeTwoList {
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+
+        if (list1 != null) {
+            tail.next = list1;
+        } else {
+            tail.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode merged = mergeTwoLists(l1, l2);
+
+        System.out.print("Merged List: ");
+        printList(merged);
+    }
+}
