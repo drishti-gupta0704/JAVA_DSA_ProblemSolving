4387bca71465623dfb6839c4b0369a4b196773ef
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-04-08 21:07:48 +0530

    solved the detection of cycle in LL problem

---
+++ src/linkedlist/DetectCycle.java
@@ -0,0 +1,50 @@
+package linkedlist;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class DetectCycle {
+
+    public static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node0 = new ListNode(0);
+        ListNode node4 = new ListNode(-4);
+
+
+        head.next = node2;
+        node2.next = node0;
+        node0.next = node4;
+        node4.next = node2;
+
+        boolean result = hasCycle(head);
+        System.out.println("Cycle exists: " + result);
+    }
+}
